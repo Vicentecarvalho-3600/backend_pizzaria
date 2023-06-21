bdf3c50492efa2722a35cbf2ee18626c94b806ab
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2023-06-21 02:50:22 -0300

    adicionado tratamento de erro e configuracao do prisma

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,12 @@
 
 
-import {Router, Request, Response} from 'express';
+import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response)=>{
-    return res.json({ok: true});
+router.get('/teste', (req: Request, res: Response) => {
+    // throw new Error('erro ao fazer essa requisição');
+    return res.json({ nome: "Vicente Carvalho" });
 });
 
 export { router };
