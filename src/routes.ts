77b8dda2e96a39f94e6ae07a2f7ffaa8fb547e93
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2023-06-23 21:38:32 -0300

    criando middleware de autenticação

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,11 @@
 import { Router} from 'express';
 
+
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+
+import { isAuthenticaded } from './middlewares/isAuthenticaded';
 
 const router = Router();
 
@@ -9,5 +13,7 @@ const router = Router();
 router.post('/users', new CreateUserController().handle)
 
 router.post('/session', new AuthUserController().handle)
+
+router.get('/me', isAuthenticaded, new DetailUserController().handle)
 
 export { router };
